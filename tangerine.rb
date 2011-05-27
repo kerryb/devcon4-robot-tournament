cca75262c2924edba37d734aa021a6b160aec41e
Kerry Buckley <kerryjbuckley@gmail.com>
2011-05-27 10:42:01 +0100

    Step through all unexplored squares

--- tangerine.rb
+++ tangerine.rb
@@ -1,8 +1,22 @@
-case ARGV[0]
+require "pp"
+
+def play arg
+  board = parse_board arg
+  row = board.find_index {|row| row.include?("-") }
+  column = board[row].find_index "-"
+  x = %w{A B C D E F G H I}[column]
+  y = row + 1
+  puts "#{x}#{y}"
+end
+
+def parse_board str
+  str.chars.each_slice(9).to_a
+end
+
+arg = ARGV[0]
+case arg
 when "START"
   puts
 else
-  x = %w{A B C D E F G H I}[rand 9]
-  y = rand(9) + 1
-  puts "#{x}#{y}"
+  play arg
 end
