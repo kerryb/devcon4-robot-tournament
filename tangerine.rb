82247e80e1b8bffa41671f259aff0bf4b46abc7c
Kerry Buckley <kerryjbuckley@gmail.com>
2011-05-27 10:53:20 +0100

    Refactor

--- tangerine.rb
+++ tangerine.rb
@@ -1,14 +1,25 @@
 def play arg
-  board = parse_board arg
-  row, column = choose_move board
+  $board = parse_board arg
+  row, column = choose_move
   x = %w{A B C D E F G H I}[column]
   y = row + 1
   puts "#{x}#{y}"
 end
 
-def choose_move board
-  row = board.find_index {|row| row.include?("-") }
-  column = board[row].find_index "-"
+def choose_move
+  find_half_killed_ships or random_fire
+end
+
+def find_half_killed_ships
+end
+
+def random_fire
+  find_char "-"
+end
+
+def find_char char
+  row = $board.find_index {|row| row.include?(char) } or return nil
+  column = $board[row].find_index(char) or return nil
   [row, column]
 end
 
