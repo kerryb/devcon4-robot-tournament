2c4b7f8027217ee35c27171777d78e61b92018dd
Kerry Buckley <kerryjbuckley@gmail.com>
2011-05-27 10:44:29 +0100

    Factor out move selection

--- tangerine.rb
+++ tangerine.rb
@@ -1,12 +1,15 @@
-require "pp"
-
 def play arg
   board = parse_board arg
-  row = board.find_index {|row| row.include?("-") }
-  column = board[row].find_index "-"
+  row, column = choose_move board
   x = %w{A B C D E F G H I}[column]
   y = row + 1
   puts "#{x}#{y}"
+end
+
+def choose_move board
+  row = board.find_index {|row| row.include?("-") }
+  column = board[row].find_index "-"
+  [row, column]
 end
 
 def parse_board str
